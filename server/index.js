784014a5d4e5a5f5213a701f042efad56e643878
dpietraszko <kdpietraszko@wp.pl>
2022-07-04 22:37:25 +0200

    Connect mssql

--- server/index.js
+++ server/index.js
@@ -1,10 +1,25 @@
 const express = require("express");
-var config = require('./dbconfig');
+const config = require('./dbconfig');
 const mssql = require("mssql");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+
+app.post("/register", (req, res) => {
+
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const db = mssql.connect(config);
+  db.query("INSERT INTO users (username, password) VALUES (?,?)",[username, password],
+    (err, result) => {
+      console.log(err);
+    }
+  );
+});
 
 
 app.listen(3001, () => {
